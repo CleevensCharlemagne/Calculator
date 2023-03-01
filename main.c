49849be8d7e80c3131a6cafb334ddcff9c42ccdb
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-02-28 22:41:15 -0500

    update

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
+//Calculator program
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int menu; //Variable for the number the user inputs
+    float num1, num2, result; //Float variables for the user input and output, used floats in case the user enters e.g. 14.7
+
+
     return 0;
 }
+//End of code
