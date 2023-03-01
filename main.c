a198ec27d7f0f26256dee2ba9d557b101af718de
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-02-28 22:42:19 -0500

    update

--- main.c
+++ main.c
@@ -36,6 +36,21 @@ int main()
 
         printf("%.2f + %.2f = %.2f\n", num1, num2, result); //Addition output
         break;
+    case 2:
+        printf("You entered Subtraction\n\n");
+
+        printf("Enter first number: "); //User input for first number
+        scanf("%f", &num1);
+
+        printf("Enter second number: "); //User input for second number
+        scanf("%f", &num2);
+
+        printf("\n");
+
+        result = num1 - num2; //Subtraction calculation
+
+        printf("%.2f - %.2f = %.2f\n", num1, num2, result); //Subtraction output
+        break;
 
     return 0;
 }
