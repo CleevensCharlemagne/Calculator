0f8dcfb17d328ae923596b671cfe06da57bc6685
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-02-28 22:41:32 -0500

    update

--- main.c
+++ main.c
@@ -7,6 +7,13 @@ int main()
     int menu; //Variable for the number the user inputs
     float num1, num2, result; //Float variables for the user input and output, used floats in case the user enters e.g. 14.7
 
+    printf("Enter a number from the list below\n\n");
+
+    printf("1. Addition\n"); //Calculator menu, user must enter a value from 1 - 4 for the program to work
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n\n");
+
 
     return 0;
 }
